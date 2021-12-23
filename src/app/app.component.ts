097457f8ea57c6ea364e9d8f29cf3d37a37c4af3
josephndegwa <kariukindegwa98@gmail.com>
2021-12-23 11:50:25 +0300

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotes';
+  quotes: string[] ;
+
+  constructor(){
+    this.quotes = ['Maina','Susan','Rachel']
+  }
 }
