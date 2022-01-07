b4d6b7339e5316b1e748f72a2adbc2850d0b10f5
josephndegwa <kariukindegwa98@gmail.com>
2022-01-07 14:44:27 +0300

    add form logic

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { PostComponent } from '../post/post.component';
+import { Quote } from '@angular/compiler';
+
+
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
+
 export class FormComponent implements OnInit {
+
 
   constructor() { }
 
